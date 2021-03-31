604318e2fb79ff9000218bdac1c102d8f2002599
perillaroc <perillaroc@gmail.com>
2021-03-31 14:59:00 +0800

    :sparkles: add more data config for station.

--- nuwe_cimiss/station.py
+++ nuwe_cimiss/station.py
@@ -14,7 +14,15 @@ logger = logging
 STATION_CONFIG = {
     "SURF_CHN_MUL_HOR": {
         "long_name": "中国地面逐小时资料",
-        "elements": "Station_Id_d,Lat,Lon,Alti,Day,Hour,PRS_Sea,TEM,DPT,WIN_D_INST,WIN_S_INST,PRE_1h,PRE_6h,PRE_24h,PRS"
+        "elements": "Station_Id_d,Lat,Lon,Alti,Year,Mon,Day,Hour,PRS_Sea,TEM,DPT_,WIN_D_INST,WIN_S_INST,PRE_1h,PRE_6h,PRE_24h,PRS"
+    },
+    "SURF_CHN_MUL_DAY": {
+        "long_name": "中国地面日值数据",
+        "elements": "Station_Id_d,Lat,Lon,Alti,Year,Mon,Day,PRS_Sea_Avg,TEM_Avg,TEM_Max,TEM_Min"
+    },
+    "SURF_CHN_MUL_MON": {
+        "long_name": "中国地面月值数据（区域站）",
+        "elements": "Station_Id_d,Lat,Lon,Alti,Year,Mon,PRS_Sea_Avg,TEM_Avg,TEM_Max_Avg,TEM_Min_Avg"
     }
 }
 
@@ -98,6 +106,8 @@ def retrieve_obs_station(
 
     client = _get_client(config_file)
     result = client.callAPI_to_array2D(interface_id, params)
+    if result.request.error_code != 0:
+        logger.warning(f"request error {result.request.error_code}: {result.request.error_message}")
 
     df = result.to_pandas()
     return df

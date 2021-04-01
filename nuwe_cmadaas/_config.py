ade70d30a43bfa5483997c369a3dee7e2e0a50eb
perillaroc <perillaroc@gmail.com>
2021-04-01 14:52:28 +0800

    :sparkles: check config file path in environment variable and user home config directory.

--- nuwe_cmadaas/_config.py
+++ nuwe_cmadaas/_config.py
@@ -1,7 +1,23 @@
+import typing
+import os
+import pathlib
+
 import yaml
 
 
-def load_cmadaas_config(file_path):
-    with open(file_path) as f:
+def load_cmadaas_config(file_path: typing.Optional[str] = None):
+    path = _get_cmadaas_config_path(file_path)
+    with open(path) as f:
         config = yaml.safe_load(f)
         return config["cmadaas"]
+
+
+def _get_cmadaas_config_path(file_path: typing.Optional[str] = None):
+    if file_path is None:
+        if "NWPC_OPER_CONFIG" in os.environ:
+            return os.environ["NWPC_OPER_CONFIG"]
+        path = pathlib.Path(pathlib.Path.home(), ".config/nwpc-oper.yaml")
+        return path
+    else:
+        return file_path
+
